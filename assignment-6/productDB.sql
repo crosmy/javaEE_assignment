f371d6d298658a7cf4ec5b4ed23f1cb8a1be4bd4
crosmy <105200918+crosmy@users.noreply.github.com>
2023-11-07 23:48:20 +0800

    Add files via upload

---
+++ assignment-6/productDB.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS productDB2;
+CREATE DATABASE productDB2 CHARACTER SET utf8 COLLATE utf8_general_ci;
+use productDB2;
+
+CREATE TABLE `product` (
+  `id` bigint(20) AUTO_INCREMENT NOT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `price` float NOT NULL,
+  `product_type` varchar(255) DEFAULT NULL,
+  `stock_quantity` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `supplier` (
+  `id` bigint(20)  AUTO_INCREMENT NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `product_supplier` (
+  `product_id` bigint(20) NOT NULL,
+  `supplier_id` bigint(20) NOT NULL,
+ CONSTRAINT `PK` PRIMARY KEY (`product_id`, `supplier_id`),
+  CONSTRAINT `FK1` FOREIGN KEY (`supplier_id`) REFERENCES `supplier` (`id`),
+  CONSTRAINT `FK2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
